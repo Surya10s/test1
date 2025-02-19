d36ef74aff256a0fe2d0781f34b5eb02ee5ddcb8
Surya10s <kandansuryamass@gmail.com>
2025-02-19 18:21:52 +0530

    changed env file

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337), // Ensure this is reading the dynamic port from Render
   app: {
     keys: env.array('APP_KEYS', ['yourRandomAppKey1', 'yourRandomAppKey2']),
